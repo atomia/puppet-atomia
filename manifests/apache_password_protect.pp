a86c15ad650bd24516c509c00c797beb63bdab5b
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-01-30 09:23:55 +0000

    Refactored atomia::atomiadns_powerdns and added initial tests

--- manifests/apache_password_protect.pp
+++ manifests/apache_password_protect.pp
@@ -5,11 +5,11 @@
 #
 # [$username]
 # Define a username for protection
-# (required) 
+# (required)
 #
 # [$password]
 # Defines a password for protection
-# (required) 
+# (required)
 #
 # === Examples
 #
@@ -19,40 +19,39 @@
 #}
 
 
-class atomia::apache_password_protect ($username,$password) {
+class atomia::apache_password_protect ($username, $password) {
+  $htconf = generate("/usr/bin/htpasswd", "-bn", $username, $password)
 
-        $htconf = generate("/usr/bin/htpasswd", "-bn", $username, $password)
-        file { "/etc/apache2":
-                ensure => directory,
-                owner   => root,
-                group   => root,
-                mode    => 755,
-        }
+  file { "/etc/apache2":
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => 755,
+  }
 
-        file { "/etc/apache2/conf.d":
-                ensure => directory,
-                owner   => root,
-                group   => root,
-                mode    => 755,
-                require => File["/etc/apache2"],
-        }
+  file { "/etc/apache2/conf.d":
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => 755,
+    require => File["/etc/apache2"],
+  }
 
+  file { "/etc/apache2/htpasswd.conf":
+    replace => no,
+    owner   => root,
+    group   => root,
+    mode    => 444,
+    content => $htconf,
+    require => File["/etc/apache2"],
+  }
 
-        file { "/etc/apache2/htpasswd.conf":
-                replace => no,
-                owner   => root,
-                group   => root,
-                mode    => 444,
-                content => $htconf,
-                require => File["/etc/apache2"],
-        }
-
-        file { "/etc/apache2/conf.d/passwordprotect":
-                owner   => root,
-                group   => root,
-                mode    => 440,
-                source  => "puppet:///modules/apache_password_protect/passwordprotect",
-                require => File["/etc/apache2/conf.d"],
-        }
+  file { "/etc/apache2/conf.d/passwordprotect":
+    owner   => root,
+    group   => root,
+    mode    => 440,
+    source  => "puppet:///modules/apache_password_protect/passwordprotect",
+    require => File["/etc/apache2/conf.d"],
+  }
 }
 
--- manifests/atomiadns_powerdns.pp
+++ manifests/atomiadns_powerdns.pp
@@ -1,78 +1,112 @@
+#
+# == Class: atomia::atomiadns_powerdns
+#
+# Manifest to install/configure a powerdns nameserver 
+#
+# [password]
+# Define a password for accessing atomiadns
+# (required) 
+#
+# [agent_user]
+# Defines the username for accessing atomiadns
+# (required)
+#
+# [atomia_dns_url]
+# Url of atomiadns endpoint
+# (required)
+#
+# [atomia_dns_ns_group]
+# Nameserver group to subscribe to
+# (required)
+#
+# [ssl_enabled]
+# Defines if ssl is enabled
+# (optional) Defaults to false
+#
+#
+# === Examples
+#
+# class {'atomia::atomiadns_powerdns':
+#        agent_user     => 'atomiadns',
+#        agent_password   => 'abc123',
+#        atomia_dns_url   => 'http://127.0.0.1/atomiadns',
+#        atomia_dns_ns_group => 'default'
+#}
 class atomia::atomiadns_powerdns (
-	$ssl_enabled,
-	$agent_user,
-	$agent_password,
-	$atomia_dns_url,
-	$atomia_dns_ns_group
-){
+  # If ssl should be enabled or not
+  $ssl_enabled = 0,
+  $agent_user,
+  $agent_password,
+  $atomia_dns_url,
+  $atomia_dns_ns_group) {
+  package { atomiadns-powerdns-database: ensure => present }
 
-	if $atomia_linux_software_auto_update {
-		package { atomiadns-powerdns-database: ensure => latest }
-		package { atomiadns-powerdnssync: ensure => latest }
-	} else {
-		package { atomiadns-powerdns-database: ensure => present }
-		package { atomiadns-powerdnssync: ensure => present }
-	}
+  package { atomiadns-powerdnssync: ensure => present }
 
-	package { pdns-static: ensure => present, require => [ Service["atomiadns-powerdnssync"] ] } 
+  package { pdns-static:
+    ensure  => present,
+    require => [Service["atomiadns-powerdnssync"]]
+  }
 
-	if $operatingsystem == "Debian" {
-		package { dnsutils: ensure => present }
-	}
-	else {
-		package { bind-utils: ensure => present }
-	}
+  if $operatingsystem == "Debian" {
+    package { dnsutils: ensure => present }
+  } else {
+    package { bind-utils: ensure => present }
+  }
 
-        service { atomiadns-powerdnssync:
-                name => atomiadns-powerdnssync,
-                ensure => running,
-		pattern => ".*powerdnssync.*",
-		require => [ Package["atomiadns-powerdns-database"], Package["atomiadns-powerdnssync"], File["/etc/atomiadns.conf.powerdnssync"] ],
-		subscribe => [ File["/etc/atomiadns.conf.powerdnssync"] ],
-        }
+  service { atomiadns-powerdnssync:
+    name      => atomiadns-powerdnssync,
+    ensure    => running,
+    pattern   => ".*powerdnssync.*",
+    require   => [
+      Package["atomiadns-powerdns-database"],
+      Package["atomiadns-powerdnssync"],
+      File["/etc/atomiadns.conf.powerdnssync"]],
+    subscribe => [File["/etc/atomiadns.conf.powerdnssync"]],
+  }
 
-	if $ssl_enabled == '1' {
-                file { "/etc/atomiadns-mastercert.pem":
-                        owner   => root,
-                        group   => root,
-                        mode    => 440,
-			source => "puppet:///modules/atomia/atomiadns_powerdns/atomiadns_cert"
-                }
+  if $ssl_enabled == '1' {
+    file { "/etc/atomiadns-mastercert.pem":
+      owner  => root,
+      group  => root,
+      mode   => 440,
+      source => "puppet:///modules/atomia/atomiadns_powerdns/atomiadns_cert"
+    }
 
-		$atomiadns_conf = generate("/etc/puppet/modules/atomiadns_powerdns/files/generate_conf.sh", $agent_user, $agent_password, $fqdn, $atomia_dns_url, "ssl")
-	} else {
-		$atomiadns_conf = generate("/etc/puppet/modules/atomiadns_powerdns/files/generate_conf.sh", $agent_user, $agent_password, $fqdn, $atomia_dns_url, "nossl")
-	}
+  }
 
-	exec { "/usr/bin/atomiapowerdnssync add_server $atomia_dns_ns_group && /etc/init.d/atomiadns-powerdnssync stop && /etc/init.d/atomiadns-powerdnssync start && /usr/bin/atomiapowerdnssync full_reload_online":
-		require => Package["atomiadns-powerdnssync"],
-		unless => ["/usr/bin/atomiapowerdnssync get_server"],
-	}
-        file { "/etc/atomiadns.conf.powerdnssync":
-                owner   => root,
-                group   => root,
-                mode    => 444,
-                content => $atomiadns_conf,
-                require => [ Package["atomiadns-powerdns-database"], Package["atomiadns-powerdnssync"] ],
-		notify => Exec["atomiadns_config_sync"],
-        }
-	if !defined(File["/usr/bin/atomiadns_config_sync"])
-	{
-        	file { "/usr/bin/atomiadns_config_sync":
-                	owner   => root,
-                	group   => root,
-                	mode    => 500,
-			source  => "puppet:///modules/atomiadns_powerdns/atomiadns_config_sync",
-                	require => [ Package["atomiadns-powerdns-database"], Package["atomiadns-powerdnssync"] ],
-        	}
-	        exec { "atomiadns_config_sync":
-	                refreshonly => true,
-        	        require => File["/usr/bin/atomiadns_config_sync"],
-               	 	before => Service["atomiadns-powerdnssync"],
-                	command => "/usr/bin/atomiadns_config_sync $ns_group",
-        	}
+  exec { "/usr/bin/atomiapowerdnssync add_server $atomia_dns_ns_group && /etc/init.d/atomiadns-powerdnssync stop && /etc/init.d/atomiadns-powerdnssync start && /usr/bin/atomiapowerdnssync full_reload_online"
+  :
+    require => Package["atomiadns-powerdnssync"],
+    unless  => ["/usr/bin/atomiapowerdnssync get_server"],
+  }
 
-	}
+  file { "/etc/atomiadns.conf.powerdnssync":
+    owner   => root,
+    group   => root,
+    mode    => 444,
+    content => template("atomia/atomiadns_powerdns/atomiadns.conf.powerdnssync.erb"),
+    require => [Package["atomiadns-powerdns-database"], Package["atomiadns-powerdnssync"]],
+    notify  => Exec["atomiadns_config_sync"],
+  }
+
+  if !defined(File["/usr/bin/atomiadns_config_sync"]) {
+    file { "/usr/bin/atomiadns_config_sync":
+      owner   => root,
+      group   => root,
+      mode    => 500,
+      source  => "puppet:///modules/atomiadns_powerdns/atomiadns_config_sync",
+      require => [Package["atomiadns-powerdns-database"], Package["atomiadns-powerdnssync"]],
+    }
+
+    exec { "atomiadns_config_sync":
+      refreshonly => true,
+      require     => File["/usr/bin/atomiadns_config_sync"],
+      before      => Service["atomiadns-powerdnssync"],
+      command     => "/usr/bin/atomiadns_config_sync $atomia_dns_ns_group",
+    }
+
+  }
 
 }
 
