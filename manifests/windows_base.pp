419e39def5297bc347a5032122cca06e737040df
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-02-24 14:02:04 +0100

    Added resource transformations

--- manifests/windows_base.pp
+++ manifests/windows_base.pp
@@ -31,7 +31,10 @@ class atomia::windows_base (
   $mail_bcc_list    = "",
   $mail_reply_to    = "",
   $storage_server_hostname,
-  $mail_dispatcher_interval          = "30") {
+  $mail_dispatcher_interval          = "30",
+  $should_have_resource_transformations = 0) 
+
+  {
   dism { 'NetFx3': ensure => present }
 
   # 6.1 is 2008 R2, so this matches 2012 and forward
@@ -144,6 +147,101 @@ class atomia::windows_base (
   file { 'C:\inetpub\wwwroot\empty.crl':
     ensure => 'file',
     source => "puppet:///modules/atomia/certificates/empty.crl"
+  }
+
+  # Deploy resource transformations if needed
+  if $should_have_resource_transformations == 1 {
+
+    file { 'Domainreg' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.Domainreg.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.Domainreg.erb') 
+    }
+
+    file { 'AtomiaDNS' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.Atomiadns.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.Atomiadns.erb')
+    }
+        
+    file { 'ActiveDirectory' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.ActiveDirectory.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.ActiveDirectory.erb')
+    }
+  
+    file { 'CronAgent' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.CronAgent.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.CronAgent.erb')
+    }
+
+    file { 'MySQL' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.MySQL.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.MySQL.erb')
+    }   
+
+    file { 'EC2' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.EC2.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.EC2.erb')
+    }   
+
+    file { 'Daggre' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.Daggre.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.Daggre.erb')
+    }   
+
+    file { 'Awstats' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.Awstats.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.Awstats.erb')
+    }   
+
+    file { 'FSAgent' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.FSAgent.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.FSAgent.erb')
+    }   
+
+    file { 'Webinstaller' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.Webinstaller.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.Webinstaller.erb')
+    }   
+
+    file { 'PureFTPD' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.PureFTPD.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.PureFTPD.erb')
+    }   
+
+    file { 'ApacheAgent' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.ApacheAgent.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.ApacheAgent.erb')
+    }   
+
+    file { 'PostfixAndDovecot' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.PostfixAndDovecot.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.PostfixAndDovecot.erb')
+    }
+    
+    file { 'IISCluster' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.IIS.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.IIS.erb')
+    }
+        
+    file { 'MSSQL' :
+      path    => "C:\\Program Files (x86)\\Atomia\\AutomationServer\\Common\\Transformation Files\\Resources.MSSQL.xml",
+      ensure => 'file',
+      content  => template('resource_transformations/Resources.MSSQL.erb')
+    } 
+
   }
 
 }
