1a49f132785db556a75cac8f07a704b6d8f9c8a0
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-03-13 09:26:15 +0100

    Fixed paths in get_cert_fingerprint.sh

--- files/certificates/get_cert_fingerprint.sh
+++ files/certificates/get_cert_fingerprint.sh
@@ -1,12 +1,11 @@
 echo "Automation Server Encryption: "
-openssl x509 -in /etc/puppet/modules/atomia/files/certificates/certificates/automationencrypt.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
+openssl x509 -in /etc/puppet/atomiacerts/certificates/automationencrypt.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
 
 echo "Billing Encryption: "
-openssl x509 -in /etc/puppet/modules/atomia/files/certificates/certificates/billingencrypt.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
+openssl x509 -in /etc/puppet/atomiacerts/certificates/billingencrypt.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
 
 echo "Root cert: "
-openssl x509 -in /etc/puppet/modules/atomia/files/certificates/ca.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
-
+openssl x509 -in /etc/puppet/atomiacerts/ca.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
 
 echo "Signing: "
-openssl x509 -in /etc/puppet/modules/atomia/files/certificates/certificates/stssigning.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
+openssl x509 -in /etc/puppet/atomiacerts/certificates/stssigning.crt -fingerprint -noout | sed 's/SHA1 Fingerprint='// | sed s/\://g
