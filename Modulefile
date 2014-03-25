9ae0cd702c61f01d273054604099b12480d14391
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-03-25 15:09:04 +0100

    Fixed line endings

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name    'puppet-atomia'
-version '0.1.32'
+version '0.1.33'
 source 'https://github.com/atomia/puppet-atomia'
 author 'atomia'
 license 'Apache License, Version 2.0'
--- files/lookup_variable.sh
+++ files/lookup_variable.sh
@@ -24,7 +24,7 @@ VARIABLE=$2
 TOKEN=`grep -rhi "atomia::${NAMESPACE}::${VARIABLE}" ${HIERA_PATH} | awk '{for(i=2;i<=NF;++i)print $i}' | sed 's/\"//g' | sed "s/\'//g"`
 # If token was not found in hiera look for default in module
 if [ -z "$TOKEN" ]; then
-	TOKEN=`grep -r -i "\\$${VARIABLE}.*=" ${MODULE_PATH}/${NAMESPACE}.pp | grep -Eo '["\047].*["\047]' | sed 's/\"//g' | sed s/\'//g`
+	TOKEN=`grep -r -i "\\$${VARIABLE}.*=" ${MODULE_PATH}/${NAMESPACE}.pp | head -n1 | grep -Eo '["\047].*["\047]' | sed 's/\"//g' | sed s/\'//g`
 fi
 
 echo $TOKEN 
