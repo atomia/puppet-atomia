71c191f0d5ed8f05013f5bab8f829c98a7124be5
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-05-16 14:48:15 +0200

    Added sleep to atomia installer install script

--- files/windows_base/install_atomia_installer.ps1
+++ files/windows_base/install_atomia_installer.ps1
@@ -5,7 +5,8 @@ $client = new-object System.Net.WebClient
 "Downloading Atomia Installer"
 $InstallerUrl = "http://installer.atomia.com/Atomia%20Installer-1.4.3.exe"
 $client.DownloadFile( $InstallerUrl, (Join-Path $location "AtomiaInstaller.exe") )
-Start-Process c:\install\AtomiaInstaller.exe ("/q /log AtomiaInstaller.txt /S") -wait
+Start-Process c:\install\AtomiaInstaller.exe ("/q /log AtomiaInstaller.txt /S")
+Start-Sleep -s 10
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\AtomiaInstaller.lnk")
 $Shortcut.TargetPath = "C:\Program Files (x86)\Atomia Installer\appupdaterw.exe"
