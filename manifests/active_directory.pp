100decd4c96b28a41069e6689f9bc783e98ba346
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-05-16 15:59:46 +0200

    Fixed errors in active_directory manifest and added vagrant conditional

--- manifests/active_directory.pp
+++ manifests/active_directory.pp
@@ -1,6 +1,6 @@
 # == Class: atomia::active_directory
 #
-# The purpose of this class is to setup Active directory 
+# The purpose of this class is to setup Active directory
 # It should only be applied on the main domain controller
 #
 # IMPORTANT!
@@ -24,27 +24,47 @@
 # Stefan Mortensen <stefan.mortensen@atomia.com.com>
 #
 
-class atomia::active_directory (){
-
- file { 'c:/install': ensure => 'directory' }
-
-  file { 'c:/install/add_users.ps1':
-    ensure => 'file',
-    source => "puppet:///modules/atomia/active_directory/add_users.ps1.erb"
+class atomia::active_directory (
+        $hierapath      = "/etc/puppet/hieradata",
+        $modulepath     = "/etc/puppet/modules/atomia/manifests",
+        $lookup_var     = "/etc/puppet/modules/atomia/files/lookup_variable.sh"
+) {
+  if (!defined(File['c:/install'])) {
+    file { 'c:/install': ensure => 'directory' }
   }
+
 
   file { 'c:/install/sync_time.ps1':
     ensure => 'file',
-    source => "puppet:///modules/atomia/active_directory/sync_time.ps1.erb"
+    content => template('atomia/active_directory/sync_time.ps1.erb'),
   }
   
-  exec { 'base-install':
-    command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -file c:/install/add_users.ps1',
-    creates => 'C:\install\installed'
+  if($vagrant){
+	  file { 'c:/install/add_users_vagrant.ps1':
+	    ensure => 'file',
+	    content => template('atomia/active_directory/add_users_vagrant.ps1.erb'),
+	  }  
+    exec { 'add-ad-users-vagrant':
+      command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -file c:/install/add_users.ps1',
+      creates => 'C:\install\installed',
+      require => File['c:/install/add_users.ps1'],
+    }
+  
+  } else {
+	  file { 'c:/install/add_users.ps1':
+	    ensure => 'file',
+	    content => template('atomia/active_directory/add_users.ps1.erb'),
+	  }  
+	  exec { 'add-ad-users':
+	    command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -file c:/install/add_users.ps1',
+	    creates => 'C:\install\installed',
+	    require => File['c:/install/add_users.ps1'],
+	  }
   }
 
-  exec { 'sync-time':
+  exec { 'sync-time': 
     command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -file c:/install/sync_time.ps1',
+    require => File['c:/install/sync_time.ps1'], 
   }
 }
 
