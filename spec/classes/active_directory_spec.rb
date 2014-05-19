d70408bbd6b364432bf8bd71f2074c8e9572172e
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-05-19 15:56:52 +0200

    Fixed broken spec

--- spec/classes/active_directory_spec.rb
+++ spec/classes/active_directory_spec.rb
@@ -5,6 +5,9 @@ describe 'atomia::active_directory' do
 	# minimum set of default parameters
 	let :params do
 		{
+			:hierapath		 => 'spec/hieradata',
+			:modulepath	     => "manifests",
+            :lookup_var      => "files/lookup_variable.sh"		
 		}
 	end
 	
