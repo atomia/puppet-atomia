9ae21ad697cc5f1df4881d4ee656459a388a1a7e
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-05-22 09:45:32 +0200

    Export mail nfs share in local_nfs_server.pp

--- manifests/testenvironment/local_nfs_server.pp
+++ manifests/testenvironment/local_nfs_server.pp
@@ -1,37 +1,46 @@
 # Sets up a local nfs server for test purposes with the shares required for atomia
 class atomia::testenvironment::local_nfs_server {
-	include nfs::server
-	
-	file { '/export':
-    ensure => directory,
-  }
+  include nfs::server
+
+  file { '/export': ensure => directory, }
 
   file { '/export/content':
-    ensure => directory,
+    ensure  => directory,
     require => File["/export"],
-     mode    => 755
-  }  
-  
+    mode    => 755
+  }
+
   file { '/export/configuration':
-    ensure => directory,
+    ensure  => directory,
     require => File["/export"],
-     mode    => 755
-    }
-   
+    mode    => 755
+  }
   
-	nfs::export { '/export/content':
-	  export => {
-	    # host           options
-	    '127.0.0.1' => 'rw,async,no_root_squash,no_subtree_check',
-	  }
-	}
-	
-	 nfs::export { '/export/configuration':
+  file { '/export/mail':
+    ensure  => directory,
+    require => File["/export"],
+    mode    => 755
+  }  
+
+  nfs::export { '/export/content':
     export => {
       # host           options
       '127.0.0.1' => 'rw,async,no_root_squash,no_subtree_check',
     }
   }
 
- 
+  nfs::export { '/export/configuration':
+    export => {
+      # host           options
+      '127.0.0.1' => 'rw,async,no_root_squash,no_subtree_check',
+    }
+  }
+
+  nfs::export { '/export/mail':
+    export => {
+      # host           options
+      '127.0.0.1' => 'rw,async,no_root_squash,no_subtree_check',
+    }
+  }
+  
 }
