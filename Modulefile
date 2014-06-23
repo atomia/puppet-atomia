4dd4bb3cf2d70f26d4a81909e08aa002c0550b2e
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-06-23 15:21:53 +0200

    Added parameters to nfsmount to support different types of mounts like glusterfs

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name    'puppet-atomia'
-version '0.2.16'
+version '0.2.17'
 source 'https://github.com/atomia/puppet-atomia'
 author 'atomia'
 license 'Apache License, Version 2.0'
--- manifests/nfsmount.pp
+++ manifests/nfsmount.pp
@@ -1,23 +1,32 @@
 define atomia::nfsmount(
 		$use_nfs3,
 		$mount_point,
-		$nfs_location
+		$nfs_location,
+		$nfs_type = hiera('atomia::nfsmount::nfs_type', 'nfs'),
+		$nfs_options = hiera('atomia::nfsmount::nfs_options', 'rw,noatime')
 ) {
 	if !defined(Package['nfs-common']) {
 		package { nfs-common: ensure => present }
 	}
 
-	if $use_nfs3 {
-		$fs_type = "nfs"
-	} else {
-		$fs_type = "nfs4"
+	if $nfs_type == "nfs" 
+	{
+		if $use_nfs3 {
+			$fs_type = "nfs"
+		} else {
+			$fs_type = "nfs4"
+		}
+	}
+	else
+	{
+		$fs_type = $nfs_type
 	}
 
     mount { $mount_point:
       device => $nfs_location,
       fstype => $fs_type,
       remounts => false,
-      options => "rw,noatime",
+      options => $nfs_options,
       ensure => mounted,
       require => [File[$mount_point], Package['nfs-common']],
     }
