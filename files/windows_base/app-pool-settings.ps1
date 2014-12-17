5830dafdaf649dc0bd49ec4835e16ec689c8c61c
Stefan Mortensen <stefan.mortensen@atomia.com>
2014-12-17 15:28:40 +0100

    Modify application pool and site settings to optimize load time

--- files/windows_base/app-pool-settings.ps1
+++ files/windows_base/app-pool-settings.ps1
@@ -3,16 +3,35 @@ try{
 	Get-WebApplication
 	$webapps = Get-WebApplication
 	foreach ($webapp in get-childitem IIS:\AppPools\)
-	{	
+	{
+		$name = "IIS:\AppPools\" + $webapp.name
 		if ( $webapp.name.StartsWith("sts") ){
-			
-			$name = "IIS:\AppPools\" + $webapp.name
 			$userProfiles = (Get-ItemProperty $name processModel.loadUserProfile).Value
 			if( !$userProfiles ){
 				Set-ItemProperty $name processModel.loadUserProfile true			
 			}
 		}
-	}	
+
+		$appPool = Get-Item $name
+		if($appPool.startMode -eq "OnDemand") {
+			$appPool.startMode = "AlwaysRunning"
+			Set-Item $name $appPool
+		}
+		$idleTimeout = (Get-ItemProperty $name processModel.idleTimeout).Value.Minutes
+		if($idleTimeout -ne 0) {
+			Set-ItemProperty $name processModel.idleTimeout "0"
+		}
+	}
+
+	foreach ($site in get-childitem IIS:\Sites\)
+	{
+		$name = "IIS:\Sites\" + $site.name
+		$curSite = Get-Item $name
+		if(!$curSite.applicationDefaults.preloadEnabled){
+			$curSite.applicationDefaults.preloadEnabled = "true"
+			Set-Item $name  $curSite
+		}
+	}
 
 }catch
 {
