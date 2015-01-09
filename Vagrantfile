315eeffe5af2b2d8525e71cce2fb782e303f3c5d
Stefan Mortensen <stefan.mortensen@atomia.com>
2015-01-09 14:18:58 +0100

    Added a vagrant machine for development

---
+++ Vagrantfile
@@ -0,0 +1,29 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.network "private_network", ip: "192.168.99.99"
+
+  config.ssh.forward_agent = true
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  # config.vm.synced_folder "../data", "/vagrant_data"
+
+  config.vm.provider "virtualbox" do |vb|
+  	vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+
+  config.vm.provision "puppet" do |puppet|
+     puppet.manifests_path = "vagrant/manifests"
+     puppet.manifest_file  = "default.pp"
+     puppet.module_path = "vagrant/modules"
+  end
+end
---
+++ vagrant/modules/puppet/manifests/init.pp
@@ -0,0 +1,63 @@
+# == Class: puppet
+# This class install and manages Puppet and requirements
+#
+# === Parameters
+#
+# === Actions
+#
+# === Requires
+
+class puppet()
+{
+  package {'puppet':
+    ensure => latest,
+  }
+
+  package {'git':
+    ensure  => latest,
+  }
+
+  package { 'apache2-utils':
+    ensure => latest
+  }
+
+  package { 'rubygems-integration':
+    ensure => latest,
+  }
+
+  exec { 'install-bundler':
+    command => '/usr/bin/gem install bundler',
+    require => Package['rubygems-integration'],
+  }
+
+  exec { 'run-bundle-install':
+    command => '/usr/local/bin/bundle install',
+    cwd     => '/vagrant',
+    require => Exec['install-bundler'],
+  }
+
+  exec { 'install-watchr':
+    command => '/usr/bin/gem install watchr',
+    require => Package['rubygems-integration'],
+    }
+
+  # Set up specs
+  file { '/vagrant/spec/fixtures/modules/atomia':
+    ensure  => directory
+  }
+
+  exec { 'create-module-symlinks':
+    command => '/bin/bash -c \'for i in files lib manifests templates; \
+    do ln -s ../../../../$i $i; done\'',
+    cwd     => '/vagrant/spec/fixtures/modules/atomia',
+    require => File['/vagrant/spec/fixtures/modules/atomia'],
+    creates => '/vagrant/spec/fixtures/modules/atomia/manifests'
+    }
+
+  exec { 'apt-update':
+      command => '/usr/bin/apt-get update'
+    }
+
+  Exec['apt-update'] -> Package <| |>
+
+}
