a96fe6f9d7439804d6c158c9617d77379d28734e
Jimmy Bergman <jimmy@atomia.com>
2015-10-13 11:16:00 +0200

    Update domainreg module to support arbitrary configs and remove the ugly config merge hack

--- manifests/domainreg.pp
+++ manifests/domainreg.pp
@@ -1,92 +1,84 @@
-# == Class: domainreg
-#
-# This class is used to configure Atomia Domainreg
-#
-# === Parameters
-#
-# [*domainreg_service_url*]
-#  (optional) Url for the domainreg_service. Defaults to 'http://localhost/domainreg'
-#
-# === Examples
-#
-# === Authors
-#
-# Stefan Mortensen <stefan.mortensen@atomia.com>
-#
+## Atomia Domain Registration
+
+### Deploys and configures a server running Atomia Domain Registration.
+
+### Variable documentation
+#### service_url: The URL of the Atomia Domain Registration service.
+#### service_username: The username to require for accessing the service.
+#### service_password: The password to require for accessing the service.
+#### db_hostname: The hostname of the Atomia Domain Registration database.
+#### db_username: The username for the Atomia Domain Registration database.
+#### db_password: The password for the Atomia Domain Registration database.
+#### domainreg_global_config: The global section of the /etc/domainreg.conf file.
+#### domainreg_tld_config_hash: The TLD configuration sections for all the Atomia Domain Registration TLD processes.
+
+### Validations
+##### service_url(advanced): %url
+##### service_username(advanced): ^[a-z]+$
+##### service_password(advanced): %password
+##### db_hostname(advanced): ^[a-z0-9_][a-z0-9._-]*$
+##### db_username(advanced): ^[a-z]+$
+##### db_password(advanced): %password
+##### domainreg_global_config(advanced,default_file=domainreg_global_default.conf): %domainreg_global_config
+##### domainreg_tld_config_hash: %domainreg_tld_config_hash
+
 class atomia::domainreg (
-        $service_url          		= "http://localhost/domainreg",
-        $service_username     		= "domainreg",
-        $service_password     		= "",
-        $opensrs_user  			= "opensrs",
-        $opensrs_pass  			= "",
-        $opensrs_url   			= "https://horizon.opensrs.net:55443/",
-        $opensrs_tlds  			= ["com","net","org","info"],
-        $ssl_enabled            	= 0,
-        $opensrs_only  			= 1,
-	$linux_software_auto_update 	= 0
+	$service_url	  		= "http://$fqdn/domainreg",
+	$service_username     		= "domainreg",
+	$service_password     		= "",
+	$db_hostname			= "127.0.0.1",
+	$db_username			= "domainreg",
+	$db_password			= "",
+	$domainreg_global_config	= "",
+	$domainreg_tld_config_hash	= {}
 ){
 
-        if $linux_software_auto_update {
-                package { atomiadomainregistration-masterserver: ensure => latest }
-                package { atomiadomainregistration-client: ensure => latest }
-        } else {
-                package { atomiadomainregistration-masterserver: ensure => present }
-                package { atomiadomainregistration-client: ensure => present }
-        }
+	$domainreg_global_config_default = file("atomia/domainreg_global_default.conf")
 
-        package { procmail: ensure => latest }
+	package { atomiadomainregistration-masterserver:
+		ensure => present,
+		require => [ File["/etc/domainreg.conf"] ]
+	}
 
-        if $ssl_enabled == 1 {
-                #include apache_wildcard_ssl
-        }
+	package { atomiadomainregistration-client: ensure => present }
 
-        if $opensrs_only == 1
-        {
+	package { procmail: ensure => present }
 
-                file { "domainreg.conf.puppet":
-                        path    => "/etc/domainreg.conf.puppet",
-                        owner   => root,
-                        group   => root,
-                        mode    => 444,
-                        content => template('atomia/domainreg.conf.puppet'),
-                        require => [ Package["atomiadomainregistration-masterserver"], Package["atomiadomainregistration-client"] ],
-                        notify => Exec["domainreg.conf puppetmerge"],
-                }
 
-                exec { "domainreg.conf puppetmerge":
-                        command => "/usr/bin/awk 'FILENAME == \"/etc/domainreg.conf\" && !/^db_/ { next } { print }' /etc/domainreg.conf /etc/domainreg.conf.puppet > /tmp/domainreg.conf.puppetmerge && mv /tmp/domainreg.conf.puppetmerge /etc/domainreg.conf",
-                        onlyif => "/usr/bin/test -f /etc/domainreg.conf && test -f /etc/domainreg.conf.puppet",
-                        refreshonly => true,
-                        notify => Service["atomiadomainregistration-api"],
-                }
+	file { "/etc/domainreg.conf":
+		path    => "/etc/domainreg.conf",
+		owner   => root,
+		group   => root,
+		mode    => 444,
+		content => template('atomia/domainreg.conf'),
+		notify => [ Service["atomiadomainregistration-api"], Service["apache2"] ]
+	}
 
-                service { atomiadomainregistration-api:
-                        name => atomiadomainregistration-api,
-                        enable => true,
-                        ensure => running,
-                        pattern => ".*/usr/bin/domainregistration.*",
-                        require => [ Package["atomiadomainregistration-masterserver"], Package["atomiadomainregistration-client"], File["/etc/domainreg.conf.puppet"] ],
-                }
-        }
+	service { atomiadomainregistration-api:
+		name => atomiadomainregistration-api,
+		enable => true,
+		ensure => running,
+		pattern => ".*/usr/bin/domainregistration.*",
+		require => [ Package["atomiadomainregistration-masterserver"], Package["atomiadomainregistration-client"], File["/etc/domainreg.conf"] ],
+	}
 
-        if !defined(Class['atomia::apache_password_protect']) {
-                class {
-                        'atomia::apache_password_protect':
-                                username => $service_username,
-                                password => $service_password
-                }
-        }
+	if !defined(Class['atomia::apache_password_protect']) {
+		class { 'atomia::apache_password_protect':
+			username => $service_username,
+			password => $service_password
+		}
+	}
 
-        service { apache2:
-                name => apache2,
-                enable => true,
-                ensure => running,
-                require => Package['atomiadomainregistration-masterserver'],
-        }
+	service { apache2:
+		name => apache2,
+		enable => true,
+		ensure => running,
+		require => Package['atomiadomainregistration-masterserver'],
+	}
 
-    file { '/etc/cron.d/rotate-domainreg-logs':
-        ensure  => present,
-        content => "0 0 * * * root lockfile -r0 /var/run/rotate-domainreg-logs && (find /var/log/atomiadomainregistration -mtime +14 -exec rm -f '{}' '+'; rm -f /var/run/rotate-domainreg-logs.lock)",
+	file { '/etc/cron.d/rotate-domainreg-logs':
+		ensure  => present,
+		content => "0 0 * * * root lockfile -r0 /var/run/rotate-domainreg-logs && (find /var/log/atomiadomainregistration -mtime +14 -exec rm -f '{}' '+'; rm -f /var/run/rotate-domainreg-logs.lock)",
     }
 }
 
