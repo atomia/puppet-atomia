3075e4462d06d395cec611ce4ce8beb6190e0cc2
Stefan Mortensen <stefan.mortensen@atomia.com>
2015-10-22 07:52:05 +0000

    Added parameter atomia_admin_password to config manifest

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,14 +4,15 @@
 
 ### Variable documentation
 #### atomia_domain: The domain name where all your Atomia applications will be placed. For example writing atomia.com in the box below will mean that your applications will be accessible at hcp.atomia.com, billing.atomia.com etc. Please make sure that you have a valid wildcard SSL certificate for the domain name you choose as the Atomia frontend applications are served over SSL
-
+#### atomia_admin_password: Password used to log in to Atomia Admin Panel
 
 ### Validations
 ##### atomia_domain: .*
+##### atomia_admin_password(advanced): %password
 
 class atomia::config (
-	$atomia_domain         = ""
-
+	$atomia_domain	  					= "",
+	$atomia_admin_password   		= "Administrator",
 ){
 
 }
