ec5ec22e25d9f60c26af75cf4c5783e78eae93a9
Jimmy Bergman <jimmy@atomia.com>
2015-11-03 15:49:40 +0100

    Refactor postgresql manifest to new installation structure

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -1,7 +1,18 @@
+## PostgreSQL
+
+### Deploys and configures a customer PostgreSQL server.
+
+### Variable documentation
+#### postgresql_username: The username of the PostgreSQL user that Automation Server provisions databases and users through.
+#### postgresql_password: The password for the PostgreSQL user that Automation Server provisions databases and users through.
+
+### Validations
+##### postgresql_username(advanced): ^[a-z0-9_-]+$
+##### postgresql_password(advanced): %password
+
 class atomia::postgresql (
-	$postgresql_username,
-	$postgresql_password,
-	$provisioning_host
+	$postgresql_username = "automationserver",
+	$postgresql_password
 	){
 
 	class { 'postgresql::server':
