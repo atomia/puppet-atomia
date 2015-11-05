97cd4ac3bff553d2315c77cb5df35a113d3b7115
Jimmy Bergman <jimmy@atomia.com>
2015-11-05 16:10:04 +0100

    Remove unused files

--- files/phpmyadmin/fix-debian-maint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mysql_command="mysql --defaults-file=/etc/mysql/debian.cnf -Ns"
-command=`$mysql_command -e "show columns from user like '%_priv'" mysql | /bin/grep enum | /usr/bin/cut -f1 | /usr/bin/awk '{ print $1, "= '\'Y\''" }' | /usr/bin/paste -sd ','`
-
-$mysql_command -e "UPDATE user SET $command WHERE User = 'debian-sys-maint' AND Host = 'localhost'; FLUSH PRIVILEGES;" mysql
