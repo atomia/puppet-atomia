792b1966df1afb9b3eb70df153b97c69b57e330f
Branislav Vukelic <branislav@atomia.com>
2015-11-12 02:45:55 +0100

    Fix to use an array of PHP verions

--- manifests/apache_agent.pp
+++ manifests/apache_agent.pp
@@ -5,7 +5,7 @@
 #
 # [password]
 # Define a password for accessing the agent
-# (required) 
+# (required)
 #
 # [content_share_nfs_location]
 # Define where the content nfs share is located
@@ -60,11 +60,9 @@ class atomia::apache_agent (
   $apache_agent_ip = $ipaddress,
   $maps_path = "/storage/configuration/maps",
   # Set this property to 0 if you don't want to have PHP selector feature
-  $should_have_php_farm = 1,
+  $should_have_php_farm = 0,
   # Set php_version_XX property to desired PHP version
-  $php_version_54 = "5.4.45",
-  $php_version_55 = "5.5.30",
-  $php_version_56 = "5.6.10",
+  $php_versions = ['5.4.45','5.5.29','5.6.10'],
   ) {
 
   if $lsbdistrelease == "14.04" {
@@ -73,14 +71,14 @@ class atomia::apache_agent (
     $pa_site = "000-default.conf"
     $pa_site_enabled = "000-default.conf"
   } else {
-    $pa_conf_available_path = "/etc/apache2/conf.d"   
+    $pa_conf_available_path = "/etc/apache2/conf.d"
     $pa_conf_file = "atomia-pa-apache.conf.ubuntu"
     $pa_site = "default"
     $pa_site_enabled = "000-default"
   }
-  
+
   if $should_have_pa_apache == 1 {
-    package { atomia-pa-apache: 
+    package { atomia-pa-apache:
       ensure => present,
       require => Package["apache2"],
     }
@@ -152,13 +150,13 @@ class atomia::apache_agent (
   } else {
     $ssl_generate_var = "nossl"
   }
-  
+
   atomia::nfsmount { 'mount_content':
     use_nfs3 => $use_nfs3,
     mount_point => '/storage/content',
     nfs_location => $content_share_nfs_location
   }
-  
+
   atomia::nfsmount { 'mount_config':
     use_nfs3 => $use_nfs3,
     mount_point => '/storage/configuration',
@@ -296,7 +294,7 @@ class atomia::apache_agent (
     require => File["${$pa_conf_available_path}/001-custom-errors"],
     notify  => Service["apache2"],
     }
-  } 
+  }
 
   file { "/etc/apache2/suexec/www-data":
     owner   => root,
@@ -348,15 +346,36 @@ class atomia::apache_agent (
       subscribe => [Package["atomia-pa-apache"], File["/usr/local/apache-agent/settings.cfg"]],
     }
   }
-  if ($should_have_php_farm == 1) and ($lsbdistrelease == "14.04") {
-    exec {"activate_custom_php":
-      command => '/bin/echo -e "<IF \"%{ENV:PHPVR} == \'5.5\'\">\n\tFCGIWrapper /var/www/cgi-wrappers/php-fcgid-wrapper .php\n</IF>\n" >> /etc/apache2/conf/phpversions.conf',
-      unless  => '/bin/grep -F "<IF" /etc/apache2/conf/phpversions.conf',
-      notify  => Service["apache2"],
+  
+  define arrayPHP {
+    $php_version = $name
+    # Compile PHP and create wrappers 
+    exec { "compile_php_${php_version}" :
+      command => "/opt/phpfarm/src/compile.sh ${$php_version}",
+      creates => "/opt/phpfarm/inst/bin/php-${$php_version}",
+      timeout => 1800,
+      onlyif  => "/usr/bin/test -f /opt/phpfarm/src/options.sh",
+      require => Package["libapache2-mod-fcgid-atomia"],
     }
-
+    exec {"check_php_install_${php_version}":
+      command => "/opt/phpfarm/inst/bin/php-${$php_version} --version | grep built",
+      onlyif  => "/usr/bin/test -f /opt/phpfarm/inst/bin/php-${$php_version}",
+    }
+    file { "/var/www/cgi-wrappers/php-fcgid-wrapper-${php_version}":
+      owner   => root,
+      group   => root,
+      mode    => 555,
+      content => template("atomia/apache_agent/php-fcgid-wrapper-custom.erb"),
+      require => [Exec["compile_php_${php_version}"], Exec["check_php_install_${php_version}"]],
+    }
+  }
+  
+  arrayPHP { $php_versions: }
+  
+  if ($should_have_php_farm == 1) and ($lsbdistrelease == "14.04") {
+    # Download prerequisites 
     $phpcompilepackages = [ git, libxml2, libxml2-dev, libssl-dev, libcurl4-openssl-dev, pkg-config, libicu-dev, libmcrypt-dev, php5-dev, libgeoip-dev, libmagickwand-dev, libjpeg-dev, libpng12-dev, libmysqlclient-dev ]
-    package { $phpcompilepackages: 
+    package { $phpcompilepackages:
       ensure => "installed",
       require => Package["libapache2-mod-fcgid-atomia"],
     }
@@ -372,80 +391,11 @@ class atomia::apache_agent (
       source  => "puppet:///modules/atomia/apache_agent/php-options.sh",
       require => Exec["clone_phpfarm_repo"],
     }
-    # Compile and activate PHP 5.4
-    exec { "compile_php54" :
-      command => "/opt/phpfarm/src/compile.sh ${$php_version_54}",
-      creates => "/opt/phpfarm/inst/bin/php-${$php_version_54}",
-      timeout => 1800,
-      require => [Package["libapache2-mod-fcgid-atomia"], Package["php5-dev"], File["/opt/phpfarm/src/options.sh"]],
-    }
-    exec {"check_php54_install":
-      command => "/opt/phpfarm/inst/bin/php-${$php_version_54} --version | grep built",
-      onlyif  => "/usr/bin/test -f /opt/phpfarm/inst/bin/php-${$php_version_54}",
-    }
-    file { "/var/www/cgi-wrappers/php-fcgid-wrapper-5.4":
+    file { "/etc/apache2/conf/phpversions.conf":
       owner   => root,
       group   => root,
-      mode    => 555,
-      content => template("atomia/apache_agent/php-fcgid-wrapper-54.erb"),
-      require => [Exec["compile_php54"], Exec["check_php54_install"]],
-    }
-    exec {"activate_php54":
-      command => '/bin/echo -e "<ELSEIF \"%{ENV:PHPVR} == \'5.4\'\">\n\tFCGIWrapper /var/www/cgi-wrappers/php-fcgid-wrapper-5.4 .php\n</ELSEIF>\n" >> /etc/apache2/conf/phpversions.conf',
-      unless  => "/bin/grep php-fcgid-wrapper-5.4 /etc/apache2/conf/phpversions.conf",
-      require => [File["/var/www/cgi-wrappers/php-fcgid-wrapper-5.4"], Exec["activate_custom_php"]],
-      notify  => Service["apache2"],
-    }
-
-    # Compile and activate PHP 5.5
-    exec { "compile_php55" :
-      command => "/opt/phpfarm/src/compile.sh ${$php_version_55}",
-      creates => "/opt/phpfarm/inst/bin/php-${$php_version_55}",
-      timeout => 1800,
-      require => [Package["libapache2-mod-fcgid-atomia"], Package["php5-dev"], File["/opt/phpfarm/src/options.sh"]],
-    }
-    exec {"check_php55_install":
-      command => "/opt/phpfarm/inst/bin/php-${$php_version_55} --version | grep built",
-      onlyif  => "/usr/bin/test -f /opt/phpfarm/inst/bin/php-${$php_version_55}",
-    }
-    file { "/var/www/cgi-wrappers/php-fcgid-wrapper-5.5":
-      owner   => root,
-      group   => root,
-      mode    => 555,
-      content => template("atomia/apache_agent/php-fcgid-wrapper-55.erb"),
-      require => [Exec["compile_php55"], Exec["check_php55_install"]],
-    }
-    file_line { "activate_php55":
-      path  => "/etc/apache2/conf/phpversions.conf",
-      line  => "\tFCGIWrapper /var/www/cgi-wrappers/php-fcgid-wrapper-5.5 .php",
-      match => "^.*php-fcgid-wrapper \.php",
-      require => [File["/var/www/cgi-wrappers/php-fcgid-wrapper-5.5"], Exec["activate_custom_php"]],
-      notify  => Service["apache2"],
-    }
-
-    # Compile and activate PHP 5.6
-    exec { "compile_php56" :
-      command => "/opt/phpfarm/src/compile.sh ${$php_version_56}",
-      creates => "/opt/phpfarm/inst/bin/php-${$php_version_56}",
-      timeout => 1800,
-      require => [Package["libapache2-mod-fcgid-atomia"], Package["php5-dev"], File["/opt/phpfarm/src/options.sh"]],
-    }
-    exec {"check_php56_install":
-      command => "/opt/phpfarm/inst/bin/php-${$php_version_56} --version | grep built",
-      onlyif  => "/usr/bin/test -f /opt/phpfarm/inst/bin/php-${$php_version_56}",
-    }
-    file { "/var/www/cgi-wrappers/php-fcgid-wrapper-5.6":
-      owner   => root,
-      group   => root,
-      mode    => 555,
-      content => template("atomia/apache_agent/php-fcgid-wrapper-56.erb"),
-      require => [Exec["compile_php56"], Exec["check_php56_install"]],
-    }
-    exec {"activate_php56":
-      command => '/bin/echo -e "<ELSEIF \"%{ENV:PHPVR} == \'5.6\'\">\n\tFCGIWrapper /var/www/cgi-wrappers/php-fcgid-wrapper-5.6 .php\n</ELSEIF>\n" >> /etc/apache2/conf/phpversions.conf',
-      unless  => "/bin/grep php-fcgid-wrapper-5.6 /etc/apache2/conf/phpversions.conf",
-      require => [File["/var/www/cgi-wrappers/php-fcgid-wrapper-5.6"], Exec["activate_custom_php"]],
-      notify  => Service["apache2"],
+      mode    => 644,
+      content => template("atomia/apache_agent/phpversions.erb"),
     }
   }
 
