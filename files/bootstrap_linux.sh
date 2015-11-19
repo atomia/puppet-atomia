607b37b8240da94a12a1723daa918f4e15c7102e
Stefan Mortensen <stefan.mortensen@atomia.com>
2015-11-19 09:30:23 +0000

    When bootstrapping don't try to install Puppet if it is already installed

--- files/bootstrap_linux.sh
+++ files/bootstrap_linux.sh
@@ -7,13 +7,17 @@ fi
 hostname=`hostname`
 
 dist=`cat /etc/*-release | egrep ^DISTRIB_CODENAME= | sed 's/.*=//'`
-wget http://apt.puppetlabs.com/puppetlabs-release-$dist.deb
-sudo dpkg -i puppetlabs-release-$dist.deb
-sudo apt-get update
-apt-get install -y puppet
-apt-get install -y facter
+PUPPET_INSTALLED=`dpkg -l puppet | grep puppetlabs | wc -l`
+if [ $PUPPET_INSTALLED -eq 0 ]; then
+	wget http://apt.puppetlabs.com/puppetlabs-release-$dist.deb
+	sudo dpkg -i puppetlabs-release-$dist.deb
+	sudo apt-get update
+	apt-get install -y puppet
+	apt-get install -y facter
+	sed -i 's/START=no/START=yes/' /etc/default/puppet
+fi
 
-sed -i 's/START=no/START=yes/' /etc/default/puppet
+
 
 SERVER_CONF=`grep 'ip-10-0-0-52.eu-west-1.compute.internal' /etc/puppet/puppet.conf | wc -l`
 if [ $SERVER_CONF -eq 0 ]; then
