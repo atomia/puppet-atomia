9b5e881401bfc1ea6b31e26f4b1ba6c49f3d17da
Stefan Mortensen <stefan.mortensen@atomia.com>
2015-12-09 14:45:06 +0000

    Fixes to setup and bootstrap scripts

--- files/bootstrap_linux.sh
+++ files/bootstrap_linux.sh
@@ -17,17 +17,14 @@ if [ $PUPPET_INSTALLED -eq 0 ]; then
 	sed -i 's/START=no/START=yes/' /etc/default/puppet
 fi
 
-
-
-SERVER_CONF=`grep 'ip-10-0-0-52.eu-west-1.compute.internal' /etc/puppet/puppet.conf | wc -l`
+SERVER_CONF=`grep '$1' /etc/puppet/puppet.conf | wc -l`
 if [ $SERVER_CONF -eq 0 ]; then
 	sed -i "/\[main\]/a server=$1\nlisten=true" /etc/puppet/puppet.conf
+	sed -i "/templatedir/d" /etc/puppet/puppet.conf
+	echo -e 'path /run\nallow *' >> /etc/puppet/auth.conf
+	puppet agent --enable
+	service puppet stop
 fi
-sed -i "/templatedir/d" /etc/puppet/puppet.conf
-echo -e 'path /run\nallow *' >> /etc/puppet/auth.conf
-puppet agent --enable
-service puppet stop
-
 
 #Clean up
 rm puppetlabs-release-*.deb
