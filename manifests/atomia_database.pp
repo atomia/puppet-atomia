14042b4022ea7b264626d3b18c40f714aed5ce55
Stefan Mortensen <stefan.mortensen@atomia.com>
2016-06-17 10:01:44 +0200

    Added postgresql backups to atomia_database manifest

--- manifests/atomia_database.pp
+++ manifests/atomia_database.pp
@@ -5,15 +5,25 @@
 ### Variable documentation
 #### atomia_user: The username of the PostgreSQL user that Atomia uses to connect to the database
 #### atomia_password: The password for the PostgreSQL user that Atomia uses to connect to the database
+#### server_address: The fqdn or ip address of the server
+#### enable_backups: If enabled will create a backup schedule of the PostgreSQL databases
+#### backup_dir: The directory to place the PostgreSQL backups in
+#### cron_schedule_hour: At what hour of the day should the backup be run. 1 means 1AM.
 
 ### Validations
 ##### atomia_user(advanced): ^[a-z0-9_-]+$
 ##### atomia_password(advanced): %password
+##### enable_backups(advanced): %int_boolean
+##### backup_dir(advanced): .*
+##### cron_schedule_hour(advanced): ^[0-9]{1,2}$
 
 class atomia::atomia_database (
   $atomia_user      = 'atomia',
   $atomia_password,
-  $server_address   = $fqdn
+  $enable_backups     = 1,
+  $server_address     = $fqdn,
+  $backup_dir         = '/opt/atomia_backups',
+  $cron_schedule_hour = '1'
 ){
 
   package { 'postgresql-contrib':
@@ -42,5 +52,13 @@ class atomia::atomia_database (
     address     => '0.0.0.0/0',
     auth_method => 'password',
     notify      => Service['postgresql']
+  }
+  if($enable_backups == 1 and !defined(Class['atomia::postgresql_backup'])) {
+    class {'atomia::postgresql_backup':
+      backup_dir         => $backup_dir,
+      cron_schedule_hour => $cron_schedule_hour,
+      backup_user        => $atomia_user,
+      backup_password    => $atomia_password
+    }
   }
 }
