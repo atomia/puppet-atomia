3bcfb84221bea8ca13866d40ab21ae916d74b2e2
Jimmy Bergman <jimmy@atomia.com>
2017-01-16 11:01:06 +0100

    Treat -1 as unlimited with glusterfs quota enforcement

--- files/glusterfs/quotamgmt.sh
+++ files/glusterfs/quotamgmt.sh
@@ -7,6 +7,8 @@ volume=`echo "$2" | sed -s 's/[^a-z_]//g'`
 user=`echo "$3" | sed -e 's/[^0-9]//g'`
 quota=`echo "$4" | sed -e 's/[^0-9]//g'`
 
+unlimited_quota=`if [ x"$4" = x"-1" ]; then echo "1"; else echo "0"; fi`
+
 if !([ x"$command" = x"add" ] || [ x"$command" = x"remove" ]) || [ -z "$user" ] || [ -x "$volume" ] || \
 		([ x"$command" = x"add" ] && [ -z "$quota" ]); then
 	echo "usage: $0 command volume user quotavalue"
@@ -19,6 +21,11 @@ fi
 path=`echo "$user" | sed -e 's,^[0-9]*\([0-9][0-9]\)$,/\1/\0,'`
 
 if [ x"$command" = x"add" ]; then
+	if [ x"$unlimited_quota" = x"1" ]; then
+		echo "treating -1 as unlimited by not setting quota"
+		exit 0
+	fi
+
 	output=`gluster volume quota "$volume" limit-usage "$path" "$quota" 2>&1`
 	echo "$output"
 	echo "$output" | grep -i "limit set" > /dev/null
