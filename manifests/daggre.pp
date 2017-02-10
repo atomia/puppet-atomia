ba1d67ebd5746453ac4be222f9a5deac1f5547cb
Jimmy Bergman <jimmy@atomia.com>
2017-02-10 16:01:56 +0100

    Change codename to lsbdistcodename to support both Puppet 3.8 and 4.5

--- manifests/daggre.pp
+++ manifests/daggre.pp
@@ -73,7 +73,7 @@ class atomia::daggre (
   if $::operatingsystem == 'Ubuntu' {
     apt::source { 'nodesource_0.12':
       location => 'https://deb.nodesource.com/node_0.12',
-      release  => $::codename,
+      release  => $::lsbdistcodename,
       repos    => 'main',
       key      => {
         id     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
--- manifests/fsagent.pp
+++ manifests/fsagent.pp
@@ -74,7 +74,7 @@ class atomia::fsagent (
   if $::operatingsystem == 'Ubuntu' {
     apt::source { 'nodesource_0.12':
       location => 'https://deb.nodesource.com/node_0.12',
-      release  => $::codename,
+      release  => $::lsbdistcodename,
       repos    => 'main',
       key      => {
         id     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
