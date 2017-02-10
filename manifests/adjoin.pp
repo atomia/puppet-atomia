ae9d0939b682aca033ac0f17f7f3c76347b77a12
Jimmy Bergman <jimmy@atomia.com>
2017-02-10 14:48:32 +0100

    Reindent and fix merge failure that removed last }

--- manifests/adjoin.pp
+++ manifests/adjoin.pp
@@ -27,15 +27,18 @@ class atomia::adjoin (
   $bind_user         = 'PosixGuest',
   $bind_password     = hiera('atomia::active_directory::bind_password', ''),
   $use_nss_pam_ldapd = '1',
-  ) {
+) {
+
   $active_directory_ip = hiera('atomia::active_directory::master_ip','')
   $active_directory_replica_ip = hiera('atomia::active_directory_replica::replica_ip','')
+
   if $::operatingsystem == 'windows' {
 
       file {'c:/install/update_dns.ps1':
           ensure => file,
           source => 'puppet:///modules/atomia/active_directory/update_dns.ps1',
       }
+
       if ($::domain != $domain_name) {
         exec { 'set-dns':
             command => "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -executionpolicy remotesigned -file c:/install/update_dns.ps1 ${active_directory_ip} ${active_directory_replica_ip}",
@@ -51,13 +54,6 @@ class atomia::adjoin (
         }
       }
 
-} else {
-  # Join AD on Linux
-  $dc=regsubst($domain_name, '\.', ',dc=', 'G')
-  $base_dn = "cn=Users,dc=${dc}"
-
-  if $::vagrant {
-    $ad_servers = 'ldap://192.168.33.10'
   } else {
     # Join AD on Linux
     $dc=regsubst($domain_name, '\.', ',dc=', 'G')
@@ -66,84 +62,92 @@ class atomia::adjoin (
     if $::vagrant {
       $ad_servers = 'ldap://192.168.33.10'
     } else {
-      if($active_directory_replica_ip == '') {
-        $ad_servers = "ldap://${active_directory_ip}"
+      # Join AD on Linux
+      $dc=regsubst($domain_name, '\.', ',dc=', 'G')
+      $base_dn = "cn=Users,dc=${dc}"
+
+      if $::vagrant {
+        $ad_servers = 'ldap://192.168.33.10'
       } else {
-        $ad_servers = "ldap://${active_directory_ip} ldap://${active_directory_replica_ip}"
+        if($active_directory_replica_ip == '') {
+          $ad_servers = "ldap://${active_directory_ip}"
+        } else {
+          $ad_servers = "ldap://${active_directory_ip} ldap://${active_directory_replica_ip}"
+        }
       }
-    }
 
-    if($::osfamily == 'RedHat' or $use_nss_pam_ldapd == '1') {
+      if($::osfamily == 'RedHat' or $use_nss_pam_ldapd == '1') {
 
-      if($::osfamily == 'RedHat') {
-        $nss_package_name = 'nss-pam-ldapd'
-        $nss_group = 'ldap'
+        if($::osfamily == 'RedHat') {
+          $nss_package_name = 'nss-pam-ldapd'
+          $nss_group = 'ldap'
+        } else {
+          $nss_package_name = 'libpam-ldapd'
+          $nss_group = 'nslcd'
+        }
+
+        package { $nss_package_name: ensure => present }
+
+        file { '/etc/nslcd.conf':
+          ensure  => file,
+          owner   => 'nslcd',
+          group   => $nss_group,
+          mode    => '0600',
+          content => template('atomia/adjoin/nslcd.conf.erb'),
+          require => Package[$nss_package_name],
+          notify  => Service['nslcd'],
+        }
+
+        service { 'nslcd':
+          ensure  => running,
+          enable  => true,
+          require => [ Package[$nss_package_name], File['/etc/nslcd.conf'] ],
+        }
+
       } else {
-        $nss_package_name = 'libpam-ldapd'
-        $nss_group = 'nslcd'
+
+        package { 'libpam-ldap': ensure => present }
+
+        file { '/etc/ldap.conf':
+          ensure  => file,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => template('atomia/adjoin/ldap.conf.erb'),
+        }
       }
 
-      package { $nss_package_name: ensure => present }
-
-      file { '/etc/nslcd.conf':
-        ensure  => file,
-        owner   => 'nslcd',
-        group   => $nss_group,
-        mode    => '0600',
-        content => template('atomia/adjoin/nslcd.conf.erb'),
-        require => Package[$nss_package_name],
-        notify  => Service['nslcd'],
+      file { '/etc/pam.d/common-account':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/atomia/adjoin/common-account',
       }
 
-      service { 'nslcd':
-        ensure  => running,
-        enable  => true,
-        require => [ Package[$nss_package_name], File['/etc/nslcd.conf'] ],
+      file { '/etc/nsswitch.conf':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/atomia/adjoin/nsswitch.conf',
       }
 
-    } else {
-
-      package { 'libpam-ldap': ensure => present }
-
-      file { '/etc/ldap.conf':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => template('atomia/adjoin/ldap.conf.erb'),
+      file { '/etc/pam.d/common-auth':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/atomia/adjoin/common-auth',
       }
-    }
 
-    file { '/etc/pam.d/common-account':
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/atomia/adjoin/common-account',
-    }
-
-    file { '/etc/nsswitch.conf':
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/atomia/adjoin/nsswitch.conf',
-    }
-
-    file { '/etc/pam.d/common-auth':
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/atomia/adjoin/common-auth',
-    }
-
-    file { '/etc/pam.d/common-session':
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/atomia/adjoin/common-session',
+      file { '/etc/pam.d/common-session':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/atomia/adjoin/common-session',
+      }
     }
   }
 }
