b76c557a3798fb4ef191d0fab980b1c4f498bfcc
Branislav Vukelic <branislav@atomia.com>
2017-10-10 21:56:48 +0200

    Webmail servers support added

--- manifests/webmail.pp
+++ manifests/webmail.pp
@@ -4,33 +4,81 @@
 
 ### Variable documentation
 #### cluster_ip: The virtual IP of the mail cluster.
+#### db_password: The password for the Roundcube database.
+#### mysql_root_password: The password for the MySQL root user.
 
 ### Validations
 ##### cluster_ip: %ip
+##### db_password(advanced): %password
+##### mysql_root_password(advanced): %password
 
 class atomia::webmail (
-  $cluster_ip                   = '',
+  $cluster_ip                     = '',
+  $db_password                    = '',
+  $mysql_root_password            = '',
 ) {
+
+  $mailhost = hiera('atomia::mailserver::master_ip','')
+  $publicdomain = hiera('atomia::config::atomia_domain','')
 
   class { 'apt': }
 
-  class { 'roundcube':
-    db_type     => 'pgsql',
-    db_name     => 'roundcube',
-    db_host     => 'localhost',
-    db_username => 'roundcube',
-    db_password => 'secret',
+  package { ['apache2']: ensure => present, }
+  ->
+  class { '::mysql::server':
+    restart                 => true,
+    root_password           => $mysql_root_password,
+    remove_default_accounts => true,
+  }
+  ->
+  # set debconf properties
+  exec { 'set-roundcube-password':
+    unless => "dpkg-query -l roundcube-core 2>/dev/null",
+    command => 'echo roundcube-core roundcube/mysql/app-pass password $db_password | sudo /usr/bin/debconf-set-selections',
+    path => ['/usr/bin/','/bin/'],
+  }
+  ->
+  exec { 'confirm-roundcube-password':
+    unless => "dpkg-query -l roundcube-core 2>/dev/null",
+    command => 'echo roundcube-core roundcube/app-password-confirm password $db_password | sudo /usr/bin/debconf-set-selections',
+    path => ['/usr/bin/','/bin/'],
+  }
+  ->
+  package { ['roundcube', 'roundcube-plugins', 'roundcube-plugins-extra', 'tinymce']:
+    ensure => present,
+    require => [Package['apache2'], Package['mysql-server'], ],
+  }
+  ->
+  file { '/etc/roundcube/apache.conf':
+    path    => '/etc/roundcube/apache.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('atomia/webmail/apache.conf.erb'),
+  }
+  ->
+  file { '/etc/roundcube/config.inc.php':
+    path    => '/etc/roundcube/config.inc.php',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('atomia/webmail/config.inc.php.erb'),
+    notify  => [ Service['apache2'] ]
+  }
+  ->
+  if !defined(File["/etc/apache2/sites-enabled/000-default.conf"]) {
+    file { "/etc/apache2/sites-enabled/000-default.conf":
+      ensure  => absent,
+      require => Package['apache2'],
+      notify  => Service['apache2'],
+    }
   }
 
-  package { ['apache2', 'libapache2-mod-php', 'php-pear', 'php-pgsql', 'php', 'composer', 'git', 'postgresql']: ensure => present, }
-
-  
-  file { '/opt':
-    ensure  => directory,
-  }
-  file { [ '/var/cache/puppet', '/var/cache/puppet/archives' ]:
-    ensure  => directory,
+  if !defined(Service['apache2']) {
+    service { 'apache2':
+      ensure => running,
+      enable => true,
+    }
   }
 
-  
 }
